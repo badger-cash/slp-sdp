25d36aecbd59bccb0425fb537823324c0198d32b
Vin Armani <vin@vinarmani.com>
2024-03-22 13:43:17 +1000

    v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,116 @@
 # slp-sdp
+
+Encode and encrypt SDP data according to the Simple Ledger Session Description Protocol (SLSDP)
+
+## Installation
+
+###  npm
+```shell
+npm install slp-sdp
+```
+
+###  yarn
+```shell
+yarn install slp-sdp
+```
+
+### git
+
+```shell
+git clone https://github.com/badger-cash/slp-sdp.git
+```
+  
+## Usage
+
+```js
+import * as slpsdp from "./index.js";
+
+const sdp = 'v=0\r\n' +
+'o=rtc 1074756789 0 IN IP4 127.0.0.1\r\n' +
+'s=-\r\n' +
+'t=0 0\r\n' +
+'a=group:BUNDLE 0\r\n' +
+'a=msid-semantic:WMS *\r\n' +
+'a=setup:active\r\n' +
+'a=ice-ufrag:7hJ1\r\n' +
+'a=ice-pwd:SYJg4xHfZHfWHHjCMmXIZQ\r\n' +
+'a=ice-options:ice2,trickle\r\n' +
+'a=fingerprint:sha-256 09:5B:9D:35:59:A7:1E:87:24:FF:6B:68:D3:87:E0:B6:C4:B8:4E:CD:0F:5F:76:D3:AA:8E:38:CC:A6:92:DD:D6\r\n' +
+'m=application 59405 UDP/DTLS/SCTP webrtc-datachannel\r\n' +
+'c=IN IP4 192.168.1.119\r\n' +
+'a=mid:0\r\n' +
+'a=sendrecv\r\n' +
+'a=sctp-port:5000\r\n' +
+'a=max-message-size:262144\r\n' +
+'a=candidate:1 1 UDP 2122317823 192.168.1.119 59405 typ host\r\n' +
+'a=candidate:2 1 UDP 1686109951 176.99.247.125 59405 typ srflx raddr 0.0.0.0 rport 0\r\n' +
+'a=end-of-candidates\r\n'
+
+const privateKey = Buffer.from('0551c74103024b39b51f461a55d355d558b147cbfffcc7ae24c3f88ca25d826a', 'hex')
+const publicKey = Buffer.from('0399539c7356d73a9d2e4e37f8f72539e60489f6a4753ffad3f6bf357e01ca47f9', 'hex')
+
+const encryptedSdp = slpsdp.encryptSdp(sdp, publicKey)
+const encryptedHex = encryptedSdp.toString('hex')
+const decryptedSdp = slpsdp.decryptSdp(encryptedSdp, privateKey)
+```
+
+### Produces
+
+```js
+{
+  sdp: 'v=0\r\n' +
+    'o=rtc 1074756789 0 IN IP4 127.0.0.1\r\n' +
+    's=-\r\n' +
+    't=0 0\r\n' +
+    'a=group:BUNDLE 0\r\n' +
+    'a=msid-semantic:WMS *\r\n' +
+    'a=setup:active\r\n' +
+    'a=ice-ufrag:7hJ1\r\n' +
+    'a=ice-pwd:SYJg4xHfZHfWHHjCMmXIZQ\r\n' +
+    'a=ice-options:ice2,trickle\r\n' +
+    'a=fingerprint:sha-256 09:5B:9D:35:59:A7:1E:87:24:FF:6B:68:D3:87:E0:B6:C4:B8:4E:CD:0F:5F:76:D3:AA:8E:38:CC:A6:92:DD:D6\r\n' +
+    'm=application 59405 UDP/DTLS/SCTP webrtc-datachannel\r\n' +
+    'c=IN IP4 192.168.1.119\r\n' +
+    'a=mid:0\r\n' +
+    'a=sendrecv\r\n' +
+    'a=sctp-port:5000\r\n' +
+    'a=max-message-size:262144\r\n' +
+    'a=candidate:1 1 UDP 2122317823 192.168.1.119 59405 typ host\r\n' +
+    'a=candidate:2 1 UDP 1686109951 176.99.247.125 59405 typ srflx raddr 0.0.0.0 rport 0\r\n' +
+    'a=end-of-candidates\r\n',
+  encrypted: '043345aa19d896182fb39b8722f751b560241f9cf295306b7100578147a2263664abf7eb6d1f3cc2f70f5cd62d54c50df1d97460af6fd11c0739a400792f4c6ae2c70926b5cd8bd1f160a9216488c6df8b699f9538de62bf352797078cd256722c9d29837f38c440dd00f4cb890f9e4e99d24ec23fedd9604e124d86f0322a03b40b1872c3fda9d94d599d123d12a514dd7939bff3b3f598e2a717f0653f40e3751ce503887da01b12d65eb09edb2a63b87fe94f4243d9f324365bfe14dda4db3fb24b20c62a9e39a786fd2642503b9595',
+  encryptedLength: 209,
+  decrypted: 'a=setup:active\r\n' +
+    'a=ice-ufrag:7hJ1\r\n' +
+    'a=ice-pwd:SYJg4xHfZHfWHHjCMmXIZQ\r\n' +
+    'a=fingerprint:sha-256 09:5B:9D:35:59:A7:1E:87:24:FF:6B:68:D3:87:E0:B6:C4:B8:4E:CD:0F:5F:76:D3:AA:8E:38:CC:A6:92:DD:D6\r\n' +
+    'm=application 59405 UDP/DTLS/SCTP webrtc-datachannel\r\n' +
+    'a=candidate:1 1 UDP 1686109951 176.99.247.125 59405 typ srflx raddr 0.0.0.0 rport 0\r\n'
+}
+```
+
+
+### License
+
+This software is licensed under the MIT License.
+
+Copyright Fedor Badger, 2024.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to permit
+persons to whom the Software is furnished to do so, subject to the
+following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,279 @@
+import * as eccryptoJS from '@vinarmani/eccrypto-js'
+
+const ipToBuffer = (ipString) => {
+    const ipSplit = ipString.split('.')
+    const ipBufArray = ipSplit.map(num => Buffer.alloc(1, parseInt(num)))
+    return Buffer.concat(ipBufArray)
+}
+
+const bufferToIp = (ipBuf) => {
+    const ipArr = [...ipBuf]
+    return ipArr.join('.')
+}
+
+const encodeCandidate = (candidateString) => {
+    const type = candidateString.includes('srflx') ? 1 : 2
+    const bufArr = [Buffer.alloc(1, type)]
+    const candidateSplit = candidateString.split(' ')
+    const priorityBuf = Buffer.alloc(5)
+    priorityBuf.writeUIntLE(candidateSplit[3], 0, 5)
+    const addrBuf = ipToBuffer(candidateSplit[4])
+    const portBuf = Buffer.alloc(2)
+    portBuf.writeUInt16LE(parseInt(candidateSplit[5]))
+    bufArr.push(priorityBuf, addrBuf, portBuf)
+    if (type === 1) {
+        const raddrBuf = ipToBuffer(candidateSplit[9])
+        const rportBuf = Buffer.alloc(2)
+        rportBuf.writeUInt16LE(parseInt(candidateSplit[11]))
+        bufArr.push(raddrBuf, rportBuf)
+    }
+
+    return Buffer.concat(bufArr)
+}
+
+const decodeCandidate = (encodedCandidate) => {
+    let candidateString = '1 1 UDP '
+    let offset = 0
+    const type = encodedCandidate.readUint8(offset) === 1 ? 'srflx' : 'host'
+    offset += 1
+    const priority = encodedCandidate.readUintLE(offset, 5)
+    offset += 5
+    const addr = bufferToIp(encodedCandidate.subarray(offset, offset + 4))
+    offset += 4
+    const port = encodedCandidate.readUint16LE(offset)
+    candidateString += `${priority} ${addr} ${port} typ ${type}`
+    if (type === 'srflx') {
+        offset += 2
+        const raddr = bufferToIp(encodedCandidate.subarray(offset, offset + 4))
+        offset += 4
+        const rport = encodedCandidate.readUint16LE(offset)
+        candidateString += ` raddr ${raddr} rport ${rport}`
+    }
+
+    return candidateString
+}
+
+const encodeSdp = (sdp) => {
+    const encObj = {}
+    const sdpSplit = sdp.split('\r\n')
+    for (let i = 0; i < sdpSplit.length; i++) {
+        const line = sdpSplit[i]
+        const [key, val] = line.split(line.includes('m=application') ? / (.*)/s : /:(.*)/s)
+        encObj[key] = val
+    }
+
+    let setupBuf = Buffer.alloc(1) // active, 1
+    switch (encObj['a=setup']) {
+        case 'active':
+            setupBuf.writeUint8(1)
+            break;
+        case 'passive':
+            setupBuf.writeUint8(2)
+            break;
+        case 'actpass':
+            setupBuf.writeUint8(3)
+            break;
+        default:
+            break
+    }
+
+    const ufragBuf = Buffer.from(encObj['a=ice-ufrag'], 'utf-8')
+    const ufragLenBuf = Buffer.alloc(1)
+    ufragLenBuf.writeUint8(ufragBuf.length)
+    
+    const pwdBuf = Buffer.from(encObj['a=ice-pwd'], 'utf-8')
+    const pwdLenBuf = Buffer.alloc(1)
+    pwdLenBuf.writeUInt8(pwdBuf.length)
+
+    const [, fullFingerprint] = encObj['a=fingerprint'].split(' ')
+    const fingerprint = fullFingerprint.replaceAll(':', '')
+    const fingerprintBuf = Buffer.from(fingerprint, 'hex')
+
+    const [ applicationInt ] = encObj['m=application'].split(' ', 1)
+    const applicationIntBuf = Buffer.alloc(4)
+    applicationIntBuf.writeUint32LE(applicationInt)
+
+    const candidateBuf = encodeCandidate(encObj['a=candidate'])
+
+    const encArr = [
+        setupBuf,
+        ufragLenBuf,
+        ufragBuf,
+        pwdLenBuf,
+        pwdBuf,
+        fingerprintBuf,
+        applicationIntBuf,
+        candidateBuf
+    ]
+
+    const encodedSdp = Buffer.concat(encArr)
+    return encodedSdp
+}
+
+const decimalToHex = (d, padding) => {
+    var hex = Number(d).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+    while (hex.length < padding) {
+        hex = "0" + hex;
+    }
+
+    return hex;
+}
+
+const decodeSdp = (encodedSdp) => {
+    let offset = 0
+    let sdp = 'a=setup:'
+    switch (encodedSdp.readUint8(offset)) {
+        case 1:
+            sdp += 'active'
+            break;
+        case 2:
+            sdp += 'passive'
+            break;
+        case 3:
+            sdp += 'actpass'
+            break;
+        default:
+            break
+    }
+    
+    sdp += '\r\na=ice-ufrag:'
+    offset += 1
+    const ufragLen = encodedSdp.readUint8(offset)
+    offset += 1
+    sdp += encodedSdp.subarray(offset, offset + ufragLen).toString('utf-8')
+    offset += ufragLen
+
+    sdp += '\r\na=ice-pwd:'
+    const pwdLen = encodedSdp.readUint8(offset)
+    offset += 1
+    sdp += encodedSdp.subarray(offset, offset + pwdLen).toString('utf-8')
+    offset += pwdLen
+
+    sdp += '\r\na=fingerprint:sha-256 '
+    const fingerprintBuf = encodedSdp.subarray(offset, offset + 32)
+    const fingerprintArr = [...fingerprintBuf].map(num => decimalToHex(num).toUpperCase())
+    sdp += fingerprintArr.join(':')
+    offset += 32
+
+    sdp += '\r\nm=application '
+    const applicationInt = encodedSdp.readUint32LE(offset)
+    sdp += applicationInt + ' UDP/DTLS/SCTP webrtc-datachannel'
+    offset += 4
+
+    sdp += '\r\na=candidate:'
+    const encodedCandidate = encodedSdp.subarray(offset)
+    sdp += decodeCandidate(encodedCandidate) + '\r\n'
+
+    return sdp
+
+}
+
+const invalidIpStrings = [
+    '127.0.0.1',
+    '192.168',
+    '169.254',
+    '::'
+]
+
+const extraSdp = [
+    'o=',
+    'c=',
+    's=',
+    'v=',
+    't=',
+    'a=group',
+    'a=msid',
+    'a=ice-options',
+    'a=mid',
+    'a=sendrecv',
+    'a=sctp-port',
+    'a=max-message-size'
+]
+
+const isRemoteIpCandidate = (candidateLine) => {
+    const lineSplit = candidateLine.split(' ')
+    if (lineSplit[2].toUpperCase() !== 'UDP')
+        return false
+    // if (invalidIpStrings.some(sub => lineSplit[4].includes(sub)))
+    //     return false
+    if (lineSplit[7].toUpperCase() !== 'SRFLX')
+        return false
+    return true
+}
+
+// Only use remote candidate
+const filterSdp = (sdp) => {
+    const sdpSplit = sdp.split('\r\n')
+    const filteredSplitSdp = []
+    for (let i = 0; i < sdpSplit.length; i++) {
+        const line = sdpSplit[i]
+        if (extraSdp.some(prop => line.includes(prop)))
+            continue
+        // Only include remote candidates
+        if (line.includes('a=candidate') && !isRemoteIpCandidate(line))
+            continue
+
+        filteredSplitSdp.push(line)
+        // leave after the first remote candidate
+        if (line.includes('a=candidate') && isRemoteIpCandidate(line))
+            break
+    }
+    const filteredSdp = filteredSplitSdp.join('\r\n')
+    return filteredSdp
+}
+
+const convertToEncryptStruct = (encbuf) => {
+    let offset = 0;
+    let tagLength = 32;
+    let pub;
+    switch(encbuf[0]) {
+      case 4:
+        pub = encbuf.slice(0, 65);
+        break;
+      case 3:
+      case 2:
+        pub = encbuf.slice(0, 33);
+        break;
+      default:
+        throw new Error('Invalid type: ' + encbuf[0]);
+    }
+      offset += pub.length;
+  
+    let c = encbuf.slice(offset, encbuf.length - tagLength);
+    let ivbuf = c.slice(0, 128 / 8);
+    let ctbuf = c.slice(128 / 8);
+  
+    let d = encbuf.slice(encbuf.length - tagLength, encbuf.length);
+
+    return {
+        iv: ivbuf,
+        ephemPublicKey: pub,
+        ciphertext: ctbuf,
+        mac: d
+    }
+}
+
+const encryptSdp = (sdp, publicKey) => {
+    const filteredSdp = filterSdp(sdp)
+    const encodedSdp = encodeSdp(filteredSdp)
+    const structuredEj = eccryptoJS.encryptSync(publicKey, encodedSdp)
+    return Buffer.concat([structuredEj.ephemPublicKey, structuredEj.iv, structuredEj.ciphertext, structuredEj.mac])
+}
+
+const decryptSdp = (encryptedSdpBuf, privateKey) => {
+    const encodedSdp = eccryptoJS.decryptSync(privateKey, convertToEncryptStruct(encryptedSdpBuf))
+    return decodeSdp(encodedSdp)
+}
+
+export {
+    filterSdp,
+    isRemoteIpCandidate,
+    encodeSdp,
+    decodeSdp,
+    encodeCandidate,
+    decodeCandidate,
+    encryptSdp,
+    decryptSdp
+}
